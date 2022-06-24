04ce6873853a7f723a745bcc0157a17139c08aa8
Christian Ajibodu <70984807+Christian-03@users.noreply.github.com>
2022-06-24 01:58:21 +0100

    Add files via upload

---
+++ user.js
@@ -0,0 +1,11 @@
+let username = 'Christian Ajibodu<br>'
+let height = '1.8m<br>'
+let country = 'Nigeria'
+
+document.write(username)
+document.write(height)
+document.write(country)
+
+console.log(username)
+console.log(height)
+console.log(country)
